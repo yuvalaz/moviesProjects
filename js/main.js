591267e70f49da455d64f7922e1f3082eca1a8a5
yuvalramat <yuvalazulai2016@gmail.com>
2023-09-30 17:51:44 +0300

    bootsrap

--- js/main.js
+++ js/main.js
@@ -1,13 +1,22 @@
 console.log("that work!");
 const newBody = document.getElementById("myBody");
 newBody.innerHTML += `
-<header id="myHeader"></header>
+<header class="navbar navbar-expand-lg bg-light" id="myHeader"></header>
 `
 
 const manue = ["Home", "Movie", "About", "Search", "Favorite"];
 const theHref = ["index.html", "movie.html", "about.html", "search.html", "favorite.html"];
+const head = document.getElementById("myHeader")
 for (let i = 0; i < 5; i++) {
-    document.getElementById("myHeader").innerHTML +=`
-    <a href="${theHref[i]}">${manue[i]}</a>
+    head.innerHTML +=`
+    <a class="nav-link" href="${theHref[i]}">${manue[i]}</a>
     `
 }
+
+head.style = `
+background-color: #ffd;
+width:100%;
+height: 10vh;
+display: flex;
+justify-content: space-around;
+`
