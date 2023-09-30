2357dedcefd9b41008485e8ee3ec4088cecb431c
yuvalramat <yuvalazulai2016@gmail.com>
2023-09-30 17:29:04 +0300

    header html

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 console.log("that work!");
+const newBody = document.getElementById("myBody");
+newBody.innerHTML += `
+<header id="myHeader"></header>
+`
+
+const manue = ["Home", "Movie", "About", "Search", "Favorite"];
+const theHref = ["index.html", "movie.html", "about.html", "search.html", "favorite.html"];
+for (let i = 0; i < 5; i++) {
+    document.getElementById("myHeader").innerHTML +=`
+    <a href="${theHref[i]}">${manue[i]}</a>
+    `
+}
